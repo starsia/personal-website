ec9d59f4150b2c7360e063e6bf8d92d36dcee280
Sia Zhong Qing <starsia93@gmail.com>
2026-08-16 12:57:57 +0800

    Fix formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,9 @@ export default function Home() {
         {siteConfig.title}
       </h1>
       <p className="max-w-xl text-lg text-foreground/70">
-        I enjoy building software that works and creates value. My expertise lies in developing full-stack applications, data pipelines and AI interfaces.
+        I enjoy building software that works and creates value. My expertise lies in developing full-stack applications, data engineering and agentic development.
 
         I currently am strengthening my skills in cloud infrastructure and operations for reliable and safe deployment.
-
-        <br></br>
-
-        I currently am specialised in database systems.
       </p>
       <p className="max-w-xl text-lg text-foreground/70">
       </p>
